365f41dc52799c624088732003059d549063b07a
Joao Costa <costa@zarv.com>
2025-04-22 13:44:13 -0300

    Add Fleet ID config

--- services.config.ts
+++ services.config.ts
@@ -5,6 +5,11 @@ export const services = [
     specUrl: 'https://api-services.zarv.com/fleet-tracking/swagger-json',
     themeConfig: {},
   },
+  {
+    slug: 'fleet-id',
+    label: 'Fleet ID',
+    specUrl: 'https://api-services.zarv.com/asset-id/v1/docs/swagger.json',
+  },
   // {
   //   slug: 'asset-id',
   //   label: 'Asset ID',
